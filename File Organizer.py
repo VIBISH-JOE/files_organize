cd60451c6ef58b34414b052e089664fcf392e064
VIBISH-JOE <vibishjoe@gmail.com>
2025-04-16 22:24:37 +0530

    first commit

---
+++ File Organizer.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+import tkinter
+import customtkinter
+
+
+path=input("Enter path: ")
+files=os.listdir(path)
+
+for file in files:
+    filename,extension=os.path.splitext(file)
+    extension = extension[1:]
+    exten=""
+    if extension in ('mp3', 'wav', 'flac', 'ogg', 'm4a', 'aac', 'raw', 'dct', 'wma', 'opus', 'ra', 'rm'):
+        exten='Music or Audio'
+    elif extension in ('mp4', 'mkv', 'flv', 'avi', 'mov', 'wmv', 'rmvb', 'm4v', 'mpg', '3gp'):
+        exten='Videos and Movies'
+    elif extension in ("xls","xlsx","xlsm","xlsb",".xlt","xltx","xltm",".xla","xlam","xll","xlw"):
+        exten='Spreadsheets'
+    elif extension in ('ppt','pptx'):
+        exten='Presentation'    
+    elif extension in ('doc', 'docx', 'pdf', 'txt', 'odt', 'rtf', 'tex', 'wps', 'wks', 'wpd'):
+        exten='Documents'
+    elif extension in ('exe', 'msi', 'app', 'dmg', 'apk', 'jar', 'deb', 'rpm', 'pkg', 'iso'):
+        exten='Softwares'
+    elif extension in ('zip', 'rar', '7z', 'tar', 'gz', 'bz2', 'xz', 'lz', 'z', 'iso'):
+        exten='Compressed files'
+    elif extension in('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'ico', 'jfif', 'webp', 'heif'):
+        exten='Photos'
+    elif extension=='ini':
+        continue
+    else:
+        exten=extension
+
+    if os.path.exists(path+'/'+exten):
+        shutil.move(path+'/'+file, path+'/'+exten+'/'+file)
+    else:
+        os.makedirs(path+'/'+exten,)
+        shutil.move(path+'/'+file, path+'/'+exten+'/'+file)
